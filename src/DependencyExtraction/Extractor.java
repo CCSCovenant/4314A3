a40549a2c0fb1da51634f95a29eb261f75501394
CCSClassBattlecruiser <623161603@qq.com>
2022-11-09 20:12:45 -0500

    add function to Extractor

--- src/DependencyExtraction/Extractor.java
+++ src/DependencyExtraction/Extractor.java
@@ -1,7 +1,23 @@
 package DependencyExtraction;
 
 public class Extractor {
+	static Extractor extractor = new Extractor();
 	public Extractor(){
 
+	}
+
+	public static Extractor getInstance() {
+		return extractor;
+	}
+
+	public Graph Code2graph(String fileDir){
+		Graph G = new Graph();
+		//TODO
+		return G;
+	}
+	public String Graph2TA(Graph G){
+		StringBuilder s = new StringBuilder();
+		//TODO
+		return s.toString();
 	}
 }
