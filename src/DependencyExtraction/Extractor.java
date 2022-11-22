822944f919b494507a4ef636e5047174a9c64650
CCSClassBattlecruiser <623161603@qq.com>
2022-11-22 15:27:12 -0500

    fix Extractor

--- src/DependencyExtraction/Extractor.java
+++ src/DependencyExtraction/Extractor.java
@@ -22,38 +22,55 @@ public class Extractor {
 	public Graph Code2graph(String fileDir){
 		Graph G = new Graph();
 		File dir = new File(fileDir);
+		addFiles(G,dir);
 		handleDir(G,dir);
 		return G;
 	}
+	public void addFiles(Graph G,File f){
+		if (f.isDirectory()){
+			for (File file:f.listFiles()){
+				addFiles(G,file);
+			}
+		}else {
+			if (f.getName().endsWith(".c")||f.getName().endsWith(".cc")||f.getName().endsWith(".h")||f.getName().endsWith(".cpp")||f.getName().endsWith(".hpp"))
+			{
+				G.addNode(f.getName());
+			}
+
+		}
+	}
+
 	public void handleDir(Graph G,File f){
 		if (f.isDirectory()){
 			for (File file:f.listFiles()){
 				handleDir(G,file);
 			}
 		}else {
-			List<String> dependedFiles = getCInclude(f);
+			if (f.getName().endsWith(".c")||f.getName().endsWith(".cc")||f.getName().endsWith(".h")||f.getName().endsWith(".cpp")||f.getName().endsWith(".hpp"))
+			{
+				List<String> dependedFiles = getCInclude(f);
 
-			for (String s:dependedFiles){
-				G.addEdge(f.getName(),s);
+				for (String s:dependedFiles){
+					G.addEdge(f.getName(),s);
+				}
 			}
+
 		}
 	}
 	public List<String> getCInclude(File file){
-		List<String> Includes = new ArrayList<>();
-		String regex = "^#include ";
-		Pattern pattern = Pattern.compile(regex);
+		List<String> Includes = new ArrayList<>();;
 
 		try {
 			BufferedReader br = new BufferedReader(new FileReader(file));
 			String line = br.readLine();
 			while (line!=null){
-				if (pattern.matcher(line).lookingAt()){
-					String dependName = line.substring(9);
-					if (dependName.charAt(0)=='<'){
-
-					}else {
-						Includes.add(dependName);
-					}
+				if (line.startsWith("#include")){
+					line = line.replace("#include","");
+					line = line.trim();
+					line = line.replace("<","");
+					line = line.replace(">","");
+					line = line.replace("\"","");
+					Includes.add(line);
 				}
 				line = br.readLine();
 			}
